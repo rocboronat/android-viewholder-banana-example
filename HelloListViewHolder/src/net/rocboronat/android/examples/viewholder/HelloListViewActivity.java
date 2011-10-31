101fa3e2d2d745abe2cab3bdb918f1349ea39831
roc boronat <roc.boronat@BCNPO084.blueIT.local>
2011-10-31 16:41:29 +0100

    Added keystore and solved FC at onItemClick.

--- HelloListViewHolder/src/net/rocboronat/android/examples/viewholder/HelloListViewActivity.java
+++ HelloListViewHolder/src/net/rocboronat/android/examples/viewholder/HelloListViewActivity.java
@@ -50,9 +50,7 @@ public class HelloListViewActivity extends ListActivity {
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				// When clicked, show a toast with the TextView text
-				Toast.makeText(getApplicationContext(),
-						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(),"Click!!", Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
